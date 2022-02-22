82da8e4ae7fc8305a124e5f1f0d88e36978f2d75
chem <chem.chikweze@gmail.com>
2022-02-21 21:10:43 -0500

    final

--- works.py
+++ works.py
@@ -9,12 +9,10 @@ from lskbucket import build_lsh_from_file_and_given_interval
 arr_precision = [0] * 25
 arr_recall = [0] * 25
 
-precision_final = []
-recall_final = []
+interval_comparison = {}
 
 def precision_for_query(interval, brute_result, lsh):
     """
-    format for both brute_result and lsh
     2b3m-wnm2#3.txt [(1.0, '2b3m-wnm2#3.txt'), (0.05454545454545454, '3fcn-e5dk#23.txt'), 
                      (0.040268456375838924, '2mtu-ysnw#1.txt'), (0.04, '265m-q8pf#1.txt'), 
                      (0.03773584905660377, '2zaf-wufx#0.txt'), (0.033962264150943396, '2ft4-4uik#1.txt'), 
@@ -36,75 +34,69 @@ def precision_for_query(interval, brute_result, lsh):
         precision = tp/(tp + fp)        
         recall = tp/(tp + fn)
         arr_precision[i] = precision
-
         arr_recall[i] = recall
         i = i+1
-    print(arr_precision)
-    print(arr_recall)
+        comparison_to_baseline.update({key: [precision,recall]})
+    
     return comparison_to_baseline
 
 
 def run_both_then_precision(interval):
     brute_result = run_bruteforce_on_interval(interval)
     lsh_result = build_lsh_from_file_and_given_interval(interval)
-    precision_for_query(interval, brute_result, lsh_result)
+    comparison_res = precision_for_query(interval, brute_result, lsh_result)
+    # interval_comparison.update(in)
+    print(comparison_res)
 
-def plot_recall_prec(arr_recall, arr_precision):
+def plot1(arr_recall, arr_precision):
+    #create precision recall curve
+    # fig, ax = plt.subplots()
+    # for i in range(25):
+    #     print(arr_recall[i], arr_precision[i])
+
+    # ax.plot(arr_recall, arr_precision, color='purple')
     plt.plot(arr_recall,arr_precision, 'o', color='black'  )
 
     #add axis labels to plot
-    plt.title('Precision-Recall Curve for Interval 1')
+    plt.title('Precision-Recall Curve')
     plt.ylabel('Precision')
     plt.xlabel('Recall')
     #display plot
     plt.show()
 
-b_arr = [0]*4
-l_arr = [0]*4
 
 def plot():
-    avg = 20
-    t_bruteforce = {}
-    t_lsh = {}
-    for i in range(avg):
-        run_both_then_precision("0.interval")
-        run_both_then_precision("1.interval")
-        run_both_then_precision("2.interval")
-        run_both_then_precision("3.interval")
 
-        for key, time_br in time_for_interval_bruteforce.items():
-            time_lr = time_for_interval_lsh.get(key)
-            if t_bruteforce.get(key) != None:
-                t_bruteforce.update({key: time_br + t_bruteforce.get(key)})
-                t_lsh.update({key: time_lr + t_lsh.get(key)})
-            else:
-                t_bruteforce.update({key: time_br})
-                t_lsh.update({key: time_lr})
+    run_both_then_precision("0.interval")
+    run_both_then_precision("1.interval")
+    run_both_then_precision("2.interval")
+    run_both_then_precision("3.interval")
 
+    t_bruteforce = time_for_interval_bruteforce
+    t_lsh = time_for_interval_lsh
+    # extract(t_bruteforce, t_lsh)
+    b_arr = [0]*4
+    l_arr = [0]*4
     i = 0
-    print((t_bruteforce.get("0.interval")))
     for key, time_b in t_bruteforce.items():
         time_l = t_lsh.get(key)
-        time_b = time_b/avg
-        time_l = time_l/avg
-        
-        b_arr[i] += time_b
-        l_arr[i] += time_l
+        b_arr[i] = time_b
+        l_arr[i] = time_l
         i = i+1
 
-    print(b_arr)
-    print(l_arr)
-    
     interval = [0,1,2,3]
     plt.plot(interval, b_arr, label = "BruteForce")
     plt.plot(interval, l_arr, label = "LSH")
-    plt.title('20 Time Average LSH vs. Time Brute Force Curve')
+    plt.title('Time LSH vs. Time Brute Force Curve')
     plt.ylabel('time')
     plt.xlabel('intervals')
     #display plot
     plt.legend()
     plt.show()
 
-# plot()
-run_both_then_precision("0.interval")
-plot_recall_prec(arr_recall, arr_precision)
+# {int: time,  int:time}
+# plot(t_bruteforce.get("0.interval"), t_lsh.get("0.interval"))
+
+
+plot1(arr_recall, arr_precision)
+plot()
